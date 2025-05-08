2e99c4170fe04147ad847fd86854bc0ef9c8f076
PIC\p10421497 <yihuang3840@gmail.com>
2025-05-08 09:47:52 +0800

    metadata

---
+++ Quartz-demo2/src/main/java/Quartz_demo2/QuartzDemo2Application.java
@@ -0,0 +1,13 @@
+package Quartz_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzDemo2Application.class, args);
+	}
+
+}
---
+++ Quartz-demo2/src/test/java/Quartz_demo2/QuartzDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Quartz_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
